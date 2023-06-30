af88d9b52cf3eb37b27907086c492169eb84ee39
Zomblard <leo.goigoux@gmail.com>
2023-06-30 13:48:45 +0200

    [Fix] Lang

--- Lang/fr.php
+++ Lang/fr.php
@@ -30,7 +30,7 @@ return [
         "test_cmwl" => "Tester la liaison CMW Link",
         "hint_cmwl_port" => 'Si vous n\'avez pas d\'autres ports de libre, utilisez le port par défaut de votre serveur, et pensez à activer l\'option "bindToDefaultPort" dans votre fichier settings.json du plugin CMWLink',
         "status" => [
-            "title" => "Status du serveur",
+            "title" => "Statut du serveur",
             "maintenance" => "Maintenance",
             "offline" => "Hors-Ligne",
             "online" => "En ligne",
