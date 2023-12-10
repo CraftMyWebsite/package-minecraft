02a2c71d57e926032ca1fec017bfd101fd8d5e2d
Teyir <60007549+Teyir@users.noreply.github.com>
2023-12-10 12:16:26 +0100

    [IMPROVES] Update to new Package class configuration

---
+++ Package.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CMW\Package\Minecraft;
+
+use CMW\Manager\Package\IPackageConfig;
+use CMW\Manager\Package\PackageMenuType;
+use CMW\Manager\Package\PackageSubMenuType;
+
+class Package implements IPackageConfig
+{
+    public function name(): string
+    {
+        return "Minecraft";
+    }
+
+    public function version(): string
+    {
+        return "1.0.0";
+    }
+
+    public function authors(): array
+    {
+        return ["Teyir"];
+    }
+
+    public function isGame(): bool
+    {
+        return true;
+    }
+
+    public function isCore(): bool
+    {
+        return false;
+    }
+
+    public function menus(): ?array
+    {
+        return [
+            new PackageMenuType(
+                lang: "fr",
+                icon: "fas fa-cube",
+                title: "Minecraft",
+                url: "minecraft/servers",
+                permission: "minecraft.servers.list",
+                subMenus: []
+            ),
+            new PackageMenuType(
+                lang: "en",
+                icon: "fas fa-cube",
+                title: "Minecraft",
+                url: "minecraft/servers",
+                permission: "minecraft.servers.list",
+                subMenus: []
+            ),
+        ];
+    }
+}
