005db143e4bb4d281757f8b54c2af9084f2ead62
Léo <69589034+Zomblard@users.noreply.github.com>
2024-07-11 17:11:59 +0200

    [IMPROVE] Shop virtual method

--- Views/Elements/commands.config.inc.view.php
+++ Views/Elements/commands.config.inc.view.php
@@ -9,25 +9,28 @@ use CMW\Utils\Website;
 /* @var string $varName */
 
 $servers = minecraftModel::getInstance()->getServers();
-
+$command = ShopItemsVirtualRequirementModel::getInstance()->getSetting($varName.'_commands',$itemId);
 ?>
-<div class="card-in-card p-3">
+<div class="">
     <div class="form-group">
         <label for="<?=$varName?>_commands">Commandes :</label>
-        <input value="<?=$cmd ?? ""?>" class="form-control" type="text" id="<?=$varName?>_commands" name="<?=$varName?>_commands" placeholder="say {player} is the best !" required>
+        <input value="<?= $command ?>" class="input" type="text" id="<?=$varName?>_commands" name="<?=$varName?>_commands" placeholder="say {player} is the best !" required>
     </div>
     <div class="form-group">
-        <h6>Serveurs ou sera executé la commande :</h6>
-        <?php foreach ($servers as $server): ?>
-            <div class="form-check form-switch">
-                <label class="form-check-label"
-                       for="server<?= $server->getServerId() ?>"><?= $server->getServerName() ?></label>
-                <input class="form-check-input" value="<?= $server->getServerId() ?>" type="checkbox" id="server<?= $server->getServerId() ?>"
-                       name="<?= $varName ?>_server<?= $server->getServerId() ?>_">
-            </div>
+        <b>Serveurs :</b><br>
+        <?php foreach ($servers as $server):
+            $serverId = ShopItemsVirtualRequirementModel::getInstance()->getSetting($varName."_server".$server->getServerId()."_",$itemId); ?>
+            <label class="toggle">
+                <p class="toggle-label"><?= $server->getServerName() ?></p>
+                <input type="checkbox" class="toggle-input" value="<?= $server->getServerId() ?>" name="<?= $varName ?>_server<?= $server->getServerId() ?>_"
+
+                <?= $server->getServerId() === (int)$serverId ? "checked" : "" ?>>
+                <div class="toggle-slider"></div>
+            </label>
         <?php endforeach; ?>
     </div>
-    <h6>A savoir.</h6>
+    <hr>
+    <b>A savoir.</b>
     <p>- Séparez vos commandes avec un pipe : | (Alt Gr + 6) si vous souhaitez en exécuter plusieurs<br>
         - Ne pas utiliser "/" dans les commandes<br>
         - Utilisez {player} pour récupérez le nom du joueur qui vote.<br>
